92617fca2eef821f4a6aac70bb880fc399b2d6c5
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-12-18 00:45:14 +0100

    feat: use class to define User model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,10 +1,28 @@
-import { DataTypes } from 'sequelize';
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
 import sequelize from 'db';
 
-const User = sequelize.define('User', {
+class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
+  declare id: CreationOptional<number>;
+  declare username: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: { type: DataTypes.INTEGER.UNSIGNED, autoIncrement: true, primaryKey: true },
   username: { type: DataTypes.STRING, allowNull: false },
   email: { type: DataTypes.STRING, allowNull: false, unique: true },
   password: { type: DataTypes.STRING, allowNull: false },
+}, {
+  tableName: 'users',
+  timestamps: true,
+  sequelize,
 });
 
 export default User;
