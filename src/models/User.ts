8217b6b441ef55d5b01b2d63de3f84c0978b81ea
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-12-18 01:45:14 +0100

    fix: postgreSQL not supporting UNSIGNED

--- src/models/User.ts
+++ src/models/User.ts
@@ -15,7 +15,7 @@ class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
 }
 
 User.init({
-  id: { type: DataTypes.INTEGER.UNSIGNED, autoIncrement: true, primaryKey: true },
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
   username: { type: DataTypes.STRING, allowNull: false },
   email: { type: DataTypes.STRING, allowNull: false, unique: true },
   password: { type: DataTypes.STRING, allowNull: false },
