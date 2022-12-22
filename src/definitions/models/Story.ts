e248b437c7f721c5cccaefb8d1744da6c72205a1
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-12-22 23:37:15 +0100

    stash

--- src/definitions/models/Story.ts
+++ src/definitions/models/Story.ts
@@ -4,24 +4,17 @@ import {
   InferAttributes,
   InferCreationAttributes,
   CreationOptional,
-  ForeignKey,
-  Association,
 } from 'sequelize';
 import sequelize from 'core/sequelize';
-import User from './User';
 
 class Story extends Model<InferAttributes<Story>, InferCreationAttributes<Story>> {
   declare id: CreationOptional<number>;
-  declare ownerId: ForeignKey<User['id']>;
+  declare ownerId: number;
   declare open: boolean;
-
-  declare static associations: {
-    owner: Association<Story, User>;
-  };
 }
 
 Story.init({
-  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  id: { type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true },
   ownerId: { type: DataTypes.INTEGER, allowNull: false },
   open: { type: DataTypes.BOOLEAN, allowNull: false },
 }, {
@@ -29,7 +22,5 @@ Story.init({
   timestamps: true,
   sequelize,
 });
-
-Story.belongsTo(User);
 
 export default Story;
--- src/definitions/models/User.ts
+++ src/definitions/models/User.ts
@@ -17,7 +17,7 @@ class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
 }
 
 User.init({
-  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  id: { type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true },
   username: { type: DataTypes.STRING, allowNull: false },
   email: { type: DataTypes.STRING, allowNull: false, unique: true },
   password: { type: DataTypes.STRING, allowNull: false },
