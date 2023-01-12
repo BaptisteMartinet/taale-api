bb530994665be2894a5218edf6d3740d67e8c767
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-01-12 19:35:27 +0100

    refactor(core/graphql): expose function

--- src/core/graphql/expose.ts
+++ src/core/graphql/expose.ts
@@ -1,20 +1,20 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { GraphQLFieldConfig, GraphQLOutputType } from 'graphql';
-import type { ModelStatic } from 'sequelize';
+import type { ModelStatic, Model } from 'sequelize';
 import type { Context } from 'core/context';
 
 import { GraphQLInt } from 'graphql';
-import ensureModelExistence from 'core/sequelize/ensureModelExistence';
+import { ensureModelExistence } from 'core/sequelize';
 import { User } from 'definitions/models';
 import { Role } from 'definitions/enums';
 
 export interface ExposeOptions {
   ensureAuth?: boolean;
   ensureAdmin?: boolean;
-  ensureSource?: ModelStatic<any>;
+  ensureSource?: ModelStatic<Model>;
 }
 
-function expose(type: GraphQLOutputType, opts?: ExposeOptions): GraphQLFieldConfig<unknown, Context> {
+function expose(type: GraphQLOutputType, opts: ExposeOptions = {}): GraphQLFieldConfig<unknown, Context> {
+  const { ensureAuth, ensureAdmin, ensureSource } = opts;
   return {
     type,
     ...(opts?.ensureSource ? { args: { id: { type: GraphQLInt } } } : null),
@@ -22,21 +22,15 @@ function expose(type: GraphQLOutputType, opts?: ExposeOptions): GraphQLFieldConf
       if (!opts)
         return {};
       const { id } = args;
-      const { ensureAuth, ensureAdmin, ensureSource } = opts;
       const { currentUser, currentUserId } = ctx;
-      if (ensureAuth && !currentUser && currentUserId) {
-        const user = await User.findByPk(currentUserId);
-        if (!user)
-          throw new Error('User does not exist');
-        ctx.currentUser = user;
-      }
+      if (ensureAuth && !currentUser && currentUserId)
+        ctx.currentUser = await ensureModelExistence(currentUserId, User);
       if (ensureAuth && !ctx.currentUser)
         throw new Error('User must be authenticated');
       if (ensureAdmin && ctx.currentUser?.role !== Role.Admin)
         throw new Error('User must be admin');
-      if (ensureSource && id) {
+      if (ensureSource && id)
         return ensureModelExistence(id, ensureSource);
-      }
       return {};
     },
   };
