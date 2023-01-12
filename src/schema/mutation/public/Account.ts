bc0be5d64f8f91a3e73dc4e82346a0888912d4c8
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-01-12 19:24:11 +0100

    feat(schema/mutation): implent client errors in AccountMutation

--- src/schema/mutation/public/Account.ts
+++ src/schema/mutation/public/Account.ts
@@ -7,6 +7,7 @@ import {
 import * as bcrypt from 'bcrypt';
 import * as jwt from 'jsonwebtoken';
 import env from 'core/env';
+import { ClientError, ClientErrorT } from 'core/errors';
 import { User } from 'definitions/models';
 import { UserType } from 'schema/output-types';
 
@@ -24,9 +25,9 @@ const AccountMutation = new GraphQLObjectType({
         const { password, ...userArgs } = args;
         const { username, email } = userArgs;
         if (await User.count({ where: { username } }) > 0)
-          throw new Error('Username already taken');
+          throw new ClientError('Username already taken', ClientErrorT.UsernameTaken);
         if (await User.count({ where: { email } }) > 0)
-          throw new Error('Email already taken');
+          throw new ClientError('Email already taken', ClientErrorT.EmailTaken);
         const hashedPassword = await bcrypt.hash(password, 10);
         await User.create({
           password: hashedPassword,
@@ -52,9 +53,9 @@ const AccountMutation = new GraphQLObjectType({
         const { email, password } = args;
         const user = await User.findOne({ where: { email } });
         if (!user)
-          throw new Error('User does not exists');
+          throw new ClientError('User does not exists', ClientErrorT.ResourceNotFound);
         if (!bcrypt.compareSync(password, user.password))
-          throw new Error('Invalid password');
+          throw new ClientError('Invalid password', ClientErrorT.InvalidPassword);
         const token = jwt.sign({ userId: user.id }, env.JWT_SECRET_KEY, { expiresIn: '7d' });
         return { user, token };
       },
