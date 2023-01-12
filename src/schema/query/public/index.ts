b7063a8fbd9f031d7fbc44f345ccf89384be8780
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-01-12 17:02:03 +0100

    feat(schema/query): implement story and stories public endpoints

--- src/schema/query/public/index.ts
+++ src/schema/query/public/index.ts
@@ -1,10 +1,27 @@
-import { GraphQLBoolean, GraphQLObjectType } from 'graphql';
+import { GraphQLInt, GraphQLList, GraphQLNonNull, GraphQLObjectType } from 'graphql';
+import { Story } from 'definitions/models';
+import { StoryType } from 'schema/output-types';
+import { ensureModelExistence } from 'core/sequelize';
 
 const PublicQuery = new GraphQLObjectType({
   name: 'PublicQuery',
   fields: {
-    test: {
-      type: GraphQLBoolean,
+    // TODO filtrer par locale et paginer ?
+    stories: {
+      type: new GraphQLList(StoryType),
+      resolve: () => Story.findAll(),
+    },
+
+    story: {
+      type: StoryType,
+      args: {
+        storyId: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+      resolve: async (source, args, ctx) => {
+        const { storyId } = args;
+        const story = await ensureModelExistence(storyId, Story);
+        return story;
+      },
     },
   },
 });
