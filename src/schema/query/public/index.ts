4237edd37b45a3b87bccf32ae384e1b9564d1659
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-01-21 22:43:14 +0100

    feat(schema/query): implement storyOfTheDay query (WIP)

--- src/schema/query/public/index.ts
+++ src/schema/query/public/index.ts
@@ -1,7 +1,10 @@
 import { GraphQLInt, GraphQLList, GraphQLNonNull, GraphQLObjectType } from 'graphql';
+import { startOfDay } from 'date-fns';
+import { Day } from 'core/utils/time';
+import sequelize, { ensureModelExistence } from 'core/sequelize';
 import { Story } from 'definitions/models';
+import { LocaleEnum } from 'definitions/enums';
 import { StoryType } from 'schema/output-types';
-import { ensureModelExistence } from 'core/sequelize';
 
 const PublicQuery = new GraphQLObjectType({
   name: 'PublicQuery',
@@ -21,6 +24,32 @@ const PublicQuery = new GraphQLObjectType({
         const { storyId } = args;
         const story = await ensureModelExistence(storyId, Story);
         return story;
+      },
+    },
+
+    storyOfTheDay: {
+      type: StoryType,
+      args: {
+        locale: { type: new GraphQLNonNull(LocaleEnum) },
+      },
+      description: 'TODO, currently not working due to postgreSQL nor supporting "random(seed);"',
+      resolve: (source, args, ctx) => {
+        const { locale } = args;
+        const randomSeed = Math.floor(Date.now() / Day);
+        return Story.findOne({
+          include: {
+            association: Story.associations.tree,
+            required: true,
+            where: {
+              open: true,
+              locale,
+            },
+          },
+          where: {
+            createdAt: { $lt: startOfDay(Date.now()) },
+          },
+          order: sequelize.fn('random', randomSeed),
+        });
       },
     },
   },
