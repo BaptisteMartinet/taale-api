995ffaae12237acf44b1d5e4d792b205fbb6504d
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-01-22 23:56:05 +0100

    feat(definitions/helpers): implement StorySentenceLink as Story is created

--- src/definitions/helpers/completion.ts
+++ src/definitions/helpers/completion.ts
@@ -1,5 +1,20 @@
 import { nbCompletionsToMarkComplete } from 'core/constants';
-import { Story, Sentence, Completion } from 'definitions/models';
+import { ensureModelExistence } from 'core/sequelize';
+import { Story, StorySentenceLink, Sentence, Completion } from 'definitions/models';
+
+async function ascendSentencesIds(sentence: Sentence): Promise<Array<number>> {
+  const ids: Array<number> = [sentence.id];
+  const limit = 1000; // TODO unhardcode this
+  let lastSentenceParentId: number | null = sentence.parentSentenceId;
+  for (let i = 0; i < limit; ++i) {
+    if (!lastSentenceParentId)
+      break;
+    const parentSentence: Sentence = await ensureModelExistence(lastSentenceParentId, Sentence);
+    ids.push(parentSentence.id);
+    lastSentenceParentId = parentSentence.parentSentenceId;
+  }
+  return ids;
+}
 
 export async function checkCompletion(sentence: Sentence): Promise<boolean> {
   const completionCount = await Completion.count({
@@ -8,9 +23,14 @@ export async function checkCompletion(sentence: Sentence): Promise<boolean> {
   if (completionCount < nbCompletionsToMarkComplete)
     return false;
   await sentence.update({ theEnd: true });
-  await Story.create({
+  const story = await Story.create({
     sentenceId: sentence.id,
     treeId: sentence.treeId,
   });
+  const sentencesIds = await ascendSentencesIds(sentence);
+  await StorySentenceLink.bulkCreate(sentencesIds.reverse().map(sentenceId => ({
+    storyId: story.id,
+    sentenceId,
+  })));
   return true;
 }
