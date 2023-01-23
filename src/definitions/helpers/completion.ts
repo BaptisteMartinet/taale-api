113142b8e571d1d6b18fab463ad1ba540b864024
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-01-23 22:19:26 +0100

    refactor(definitions/helpers): optimize and refactor ascendSentencesIds helper

--- src/definitions/helpers/completion.ts
+++ src/definitions/helpers/completion.ts
@@ -1,17 +1,19 @@
+import { QueryTypes } from 'sequelize';
 import { nbCompletionsToMarkComplete } from 'core/constants';
-import { ensureModelExistence } from 'core/sequelize';
+import sequelize from 'core/sequelize';
 import { Story, StorySentenceLink, Sentence, Completion } from 'definitions/models';
 
-async function ascendSentencesIds(sentence: Sentence): Promise<Array<number>> {
-  const ids: Array<number> = [sentence.id];
-  const limit = 1000; // TODO unhardcode this
-  let lastSentenceParentId: number | null = sentence.parentSentenceId;
-  for (let i = 0; i < limit && lastSentenceParentId !== null; ++i) {
-    const parentSentence: Sentence = await ensureModelExistence(lastSentenceParentId, Sentence);
-    ids.push(parentSentence.id);
-    lastSentenceParentId = parentSentence.parentSentenceId;
-  }
-  return ids;
+async function ascendSentencesIds(sentenceId: number): Promise<Array<number>> {
+  const records = await sequelize.query<{ id: number, parentSentenceId: number }>(`
+with recursive cte as (
+  SELECT id, parentSentenceId FROM sentences WHERE id=${sentenceId}
+  UNION ALL
+  SELECT s.id, s.parentSentenceId FROM cte
+  JOIN sentences s ON cte.parentSentenceId=s.id
+)
+select * from cte;
+`, { type: QueryTypes.SELECT });
+  return records.map(record => record.id);
 }
 
 export async function checkCompletion(sentence: Sentence): Promise<boolean> {
@@ -25,7 +27,7 @@ export async function checkCompletion(sentence: Sentence): Promise<boolean> {
     sentenceId: sentence.id,
     treeId: sentence.treeId,
   });
-  const sentencesIds = await ascendSentencesIds(sentence);
+  const sentencesIds = await ascendSentencesIds(sentence.id);
   await StorySentenceLink.bulkCreate(sentencesIds.reverse().map(sentenceId => ({
     storyId: story.id,
     sentenceId,
