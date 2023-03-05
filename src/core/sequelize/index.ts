4e66811e8ae72d0e2b9b3100d92c08404f298b16
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-03-05 12:27:39 +0100

    feat: disable logging on dev environments

--- src/core/sequelize/index.ts
+++ src/core/sequelize/index.ts
@@ -4,4 +4,4 @@ import env from 'core/env';
 export { default as ensureNotSpam } from './ensureNotSpam';
 export { default as ensureModelExistence } from './ensureModelExistence';
 
-export default new Sequelize(env.DATABASE_URL);
+export default new Sequelize(env.DATABASE_URL, { logging: env.NODE_ENV === 'dev' });
