4814b09a42d0c9a672fd3c4ab753b7d7dc5c006d
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-03-05 12:27:17 +0100

    feat: handle NODE_ENV variable

--- src/core/env.ts
+++ src/core/env.ts
@@ -5,12 +5,14 @@ export interface Env {
   DATABASE_URL: string;
   PORT?: number;
   JWT_SECRET_KEY: string;
+  NODE_ENV: string | undefined;
 }
 
 const {
   DATABASE_URL,
   PORT,
   JWT_SECRET_KEY,
+  NODE_ENV,
 } = process.env;
 
 if (!DATABASE_URL || !JWT_SECRET_KEY)
@@ -20,6 +22,7 @@ const vars: Env = {
   DATABASE_URL,
   PORT: PORT !== undefined ? parseInt(PORT) : undefined,
   JWT_SECRET_KEY,
+  NODE_ENV,
 };
 
 export default vars;
