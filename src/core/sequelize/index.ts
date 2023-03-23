d68c120a80aa4d69e3604c3e2bd70cd4b188d419
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-03-23 23:39:57 +0100

    fix(core/sequelize): setup sequlize logging function correctly

--- src/core/sequelize/index.ts
+++ src/core/sequelize/index.ts
@@ -4,4 +4,4 @@ import env from 'core/env';
 export { default as ensureNotSpam } from './ensureNotSpam';
 export { default as ensureModelExistence } from './ensureModelExistence';
 
-export default new Sequelize(env.DATABASE_URL, { logging: env.NODE_ENV === 'dev' });
+export default new Sequelize(env.DATABASE_URL, { logging: env.NODE_ENV === 'dev' ? console.log : false });
