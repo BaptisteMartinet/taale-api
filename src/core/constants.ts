a1ae37895dca7c8aacf9a266e50f351978ec0d49
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-03-30 22:03:37 +0200

    refactor(core/constants): minor

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,13 +1,12 @@
 import { Locale } from 'definitions/enums';
 
+export const DefaultLocale = Locale.EN;
+
 export const UsernameMinLength = 2;
 export const UsernameMaxLength = 20;
 export const PasswordMinLength = 4;
 
-export const PartialStoryNbSentences = 10;
-
 export const nbReportsToDeleteResource = 3;
-
 export const nbCompletionsToMarkComplete = 3;
 
-export const DefaultLocale = Locale.EN;
+export const PartialStoryNbSentences = 10;
