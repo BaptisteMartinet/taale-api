6749554d856238e10e5ea50de49cdbdb8af2cab0
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-03-30 22:18:55 +0200

    fix(definitions/helpers): minor

--- src/definitions/helpers/completion.ts
+++ src/definitions/helpers/completion.ts
@@ -1,9 +1,9 @@
-import { nbCompletionsToMarkComplete } from 'core/constants';
+import { NbCompletionsToMarkComplete } from 'core/constants';
 import { Sentence, Completion } from 'definitions/models';
 
 export async function checkCompletion(sentence: Sentence): Promise<boolean> {
   const completionCount = await Completion.count({
     where: { sentenceId: sentence.id },
   });
-  return completionCount >= nbCompletionsToMarkComplete
+  return completionCount >= NbCompletionsToMarkComplete
 }
