787aad9bdec9fd44a7a2aae0705263a413547dcc
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-04-05 23:18:55 +0200

    fix(notificaction/emails): use correct content-type header

--- src/notification/emails/utils/renderEmailHTMLPage.tsx
+++ src/notification/emails/utils/renderEmailHTMLPage.tsx
@@ -15,8 +15,7 @@ export default function renderEmailHTMLPage(args: RenderEmailArgs) {
   const html = renderToStaticMarkup(
     <html>
       <head>
-        <meta charSet="UTF-8" />
-        <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
+        <meta httpEquiv="Content-Type" content="text/html, charset=UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
