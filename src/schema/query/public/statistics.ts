89d9f8f5d32638b264a743ebe333af2b4ccefe9d
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-04-16 01:16:44 +0200

    [BRK-API] feat(schema/query): use GraphQLNonNull in public statistics query

--- src/schema/query/public/statistics.ts
+++ src/schema/query/public/statistics.ts
@@ -1,23 +1,23 @@
-import { GraphQLObjectType, GraphQLInt } from 'graphql';
+import { GraphQLObjectType, GraphQLNonNull, GraphQLInt } from 'graphql';
 import { User, Tree, Story, Sentence } from 'definitions/models';
 
 const StatisticsQuery = new GraphQLObjectType({
   name: 'StatisticsQuery',
   fields: {
     users: {
-      type: GraphQLInt,
+      type: new GraphQLNonNull(GraphQLInt),
       resolve: () => User.count(),
     },
     openTrees: {
-      type: GraphQLInt,
+      type: new GraphQLNonNull(GraphQLInt),
       resolve: () => Tree.count({ where: { open: true } }),
     },
     stories: {
-      type: GraphQLInt,
+      type: new GraphQLNonNull(GraphQLInt),
       resolve: () => Story.count(),
     },
     sentences: {
-      type: GraphQLInt,
+      type: new GraphQLNonNull(GraphQLInt),
       resolve: () => Sentence.count(),
     },
   },
