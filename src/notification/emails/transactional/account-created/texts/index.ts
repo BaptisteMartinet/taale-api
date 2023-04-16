b8597bbb705dd0a49f1bf1b86f5296d78b84334e
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-04-16 17:56:54 +0200

    fix(notifications/emails): typos

--- src/notification/emails/transactional/account-created/texts/index.ts
+++ src/notification/emails/transactional/account-created/texts/index.ts
@@ -8,7 +8,7 @@ interface Definition {
 
 const FR: Definition = {
   subject: (username) => `Bienvenue sur Taale ${username}`,
-  body: "Vous faites officielement partie de la communauté, amusez vous bien !",
+  body: "Vous faites officiellement parti de la communauté, amusez vous bien !",
 };
 
 const EN: Definition = {
