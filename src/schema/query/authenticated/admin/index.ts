373b6950cb2060709f7d68a7267129329890da1c
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-04-16 16:42:32 +0200

    feat(schema/query): improve Q.authenticated.admin non-null typings

--- src/schema/query/authenticated/admin/index.ts
+++ src/schema/query/authenticated/admin/index.ts
@@ -1,6 +1,6 @@
 import type { Context } from 'core/context';
 
-import { GraphQLObjectType } from 'graphql';
+import { GraphQLObjectType, GraphQLNonNull } from 'graphql';
 import { GraphQLNonNullList } from 'lib/graphql';
 import { User, Tree, Sentence } from 'definitions/models';
 import { UserRestrictedType, TreeType, SentenceType } from 'schema/output-types';
@@ -9,17 +9,17 @@ const AdminQuery = new GraphQLObjectType<unknown, Context>({
   name: 'AdminQuery',
   fields: {
     users: {
-      type: new GraphQLNonNullList(UserRestrictedType),
+      type: new GraphQLNonNull(new GraphQLNonNullList(UserRestrictedType)),
       resolve: () => User.findAll(),
     },
 
     trees: {
-      type: new GraphQLNonNullList(TreeType),
+      type: new GraphQLNonNull(new GraphQLNonNullList(TreeType)),
       resolve: () => Tree.findAll({ include: [Tree.associations.owner, Tree.associations.sentences] }),
     },
 
     sentences: {
-      type: new GraphQLNonNullList(SentenceType),
+      type: new GraphQLNonNull(new GraphQLNonNullList(SentenceType)),
       resolve: () => Sentence.findAll({ include: Sentence.associations.owner }),
     },
   },
