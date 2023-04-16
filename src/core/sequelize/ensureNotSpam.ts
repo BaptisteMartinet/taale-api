86ebb9c4cf7c1846cbfae86858211dec5507b49d
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-04-16 16:12:07 +0200

    feat(core/sequelize): re-enable anti-spam filter for admin users

--- src/core/sequelize/ensureNotSpam.ts
+++ src/core/sequelize/ensureNotSpam.ts
@@ -2,7 +2,6 @@ import type { ModelStatic, Model } from 'sequelize';
 
 import { Op } from 'sequelize';
 import { ClientError } from 'core/errors';
-import { Role } from 'definitions/enums';
 import { User } from 'definitions/models';
 
 export interface CheckSpamOpts {
@@ -13,8 +12,6 @@ export interface CheckSpamOpts {
 
 async function ensureNotSpam(model: ModelStatic<Model>, opts: CheckSpamOpts): Promise<void> {
   const { user, timeFrameMs, recordsLimit } = opts;
-  if (user.role === Role.Admin)
-    return;
   const nbRecords = await model.count({
     where: {
       ownerId: user.id,
