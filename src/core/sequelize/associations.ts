6d01ad12072849c6c03afa4273043534e3c77e11
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-04-18 23:10:36 +0200

    feat(core/sequelize): rework associations helpers

---
+++ src/core/sequelize/associations.ts
@@ -0,0 +1,47 @@
+import type { Model, ModelStatic } from 'sequelize';
+
+type AssociationType = 'RESTRICT' | 'CASCADE' | 'NO ACTION' | 'SET NULL' | 'SET DEFAULT';
+
+interface AssociationDefinition<M extends Model> {
+  model: ModelStatic<M>,
+  key?: string,
+  as?: string,
+  onDelete?: AssociationType,
+}
+
+interface AssociationArgs<FromModel extends Model, ToModel extends Model> {
+  from: AssociationDefinition<FromModel>,
+  to: AssociationDefinition<ToModel>,
+}
+
+export function createOneToManyAssociation<FromModel extends Model, ToModel extends Model>(args: AssociationArgs<FromModel, ToModel>) {
+  const { from, to } = args;
+  from.model.hasMany(to.model, {
+    sourceKey: from.key,
+    foreignKey: to.key,
+    as: from.as,
+    onDelete: from.onDelete,
+  });
+  to.model.belongsTo(from.model, {
+    foreignKey: to.key,
+    targetKey: from.key,
+    as: to.as,
+    onDelete: to.onDelete,
+  });
+}
+
+export function createOneToOneAssociation<FromModel extends Model, ToModel extends Model>(args: AssociationArgs<FromModel, ToModel>) {
+  const { from, to } = args;
+  from.model.hasOne(to.model, {
+    sourceKey: from.key,
+    foreignKey: to.key,
+    as: from.as,
+    onDelete: from.onDelete,
+  });
+  to.model.belongsTo(from.model, {
+    foreignKey: to.key,
+    targetKey: from.key,
+    as: to.as,
+    onDelete: to.onDelete,
+  });
+}
--- src/core/sequelize/index.ts
+++ src/core/sequelize/index.ts
@@ -3,6 +3,6 @@ import env from 'core/env';
 
 export { default as ensureNotSpam } from './ensureNotSpam';
 export { default as ensureModelExistence } from './ensureModelExistence';
-export * from './relationships';
+export * from './associations';
 
 export default new Sequelize(env.DATABASE_URL, { logging: env.NODE_ENV === 'dev' ? console.log : false });
