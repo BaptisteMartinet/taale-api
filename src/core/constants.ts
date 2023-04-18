88b4bbfe0086e702733b58c8dad3605fa419043c
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-04-18 23:39:06 +0200

    feat(core/constants): add ResetPasswordCodeLength constant

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -17,6 +17,8 @@ export const UsernameMaxLength = 24;
 export const UsernameValidationRegex = new RegExp(`^[\\w\\.]{${UsernameMinLength},${UsernameMaxLength}}$`);
 export const PasswordMinLength = 4;
 
+export const ResetPasswordCodeLength = 4;
+
 export const SentenceTextMinLength = 3;
 export const SentenceTextMaxLength = 180;
 
