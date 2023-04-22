acd4b440ee55dc13f473e91bf73fd93d8898e1fe
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-04-22 15:49:52 +0200

    feat(definitions/models): nake User.username column unique

--- src/definitions/models/User.ts
+++ src/definitions/models/User.ts
@@ -41,7 +41,7 @@ class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
 
 User.init({
   id: { type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true },
-  username: { type: DataTypes.STRING, allowNull: false, validate: { min: UsernameMinLength, max: UsernameMaxLength, is: UsernameValidationRegex } },
+  username: { type: DataTypes.STRING, allowNull: false, unique: true, validate: { min: UsernameMinLength, max: UsernameMaxLength, is: UsernameValidationRegex } },
   email: { type: DataTypes.STRING, allowNull: false, unique: true, validate: { isEmail: true } },
   password: { type: DataTypes.STRING, allowNull: false, validate: { min: PasswordMinLength } },
   role: { type: DataTypes.ENUM, allowNull: false, values: Object.values(Role), defaultValue: Role.Default },
