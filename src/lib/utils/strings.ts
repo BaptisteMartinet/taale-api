351465aaa79988e293674019bc6bab0f47453464
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-05-07 11:07:00 +0200

    feat(lib/utils): implement genAlphaNumericCode

--- src/lib/utils/strings.ts
+++ src/lib/utils/strings.ts
@@ -4,3 +4,12 @@ export function genNumericalCode(length: number) {
     code += Math.floor(Math.random() * 10);
   return code;
 }
+
+const AlphaNumericChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+export function genAlphaNumericCode(length: number) {
+  let code = '';
+  for (let i = 0; i < length; ++i)
+    code += AlphaNumericChars.charAt(Math.floor(Math.random() * AlphaNumericChars.length))
+  return code;
+}
